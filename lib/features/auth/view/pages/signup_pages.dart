89fe0bb903144af8b2c53ee7681bf2dd5be84588
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2024-08-07 22:02:58 +0700

    feat: add app pallete color

---
+++ lib/features/auth/view/pages/signup_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupPages extends StatefulWidget {
+  const SignupPages({super.key});
+
+  @override
+  State<SignupPages> createState() => _SignupPagesState();
+}
+
+class _SignupPagesState extends State<SignupPages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

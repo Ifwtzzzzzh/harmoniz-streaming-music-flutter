72786249696850e22a0fdf36a5457fd85799b251
Ifwtzzzzzh <nosarahmanda.rplb@gmail.com>
2024-08-08 11:37:48 +0700

    feat: add signup page ui, theming, and form

--- lib/features/auth/view/pages/signup_pages.dart
+++ lib/features/auth/view/pages/signup_pages.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:harmoniz/core/theme/app_pallete.dart';
+import 'package:harmoniz/features/auth/view/widgets/auth_gradient_button.dart';
+import 'package:harmoniz/features/auth/view/widgets/custom_field.dart';
 
 class SignupPages extends StatefulWidget {
   const SignupPages({super.key});
@@ -8,8 +11,69 @@ class SignupPages extends StatefulWidget {
 }
 
 class _SignupPagesState extends State<SignupPages> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up.',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 30),
+              CustomField(hintText: 'Name', controller: nameController),
+              const SizedBox(height: 15),
+              CustomField(hintText: 'Email', controller: emailController),
+              const SizedBox(height: 15),
+              CustomField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObsecureText: true,
+              ),
+              const SizedBox(height: 20),
+              const AuthGradientButton(),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: 'Already have an account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: const [
+                    TextSpan(
+                      text: 'Sign In',
+                      style: TextStyle(
+                        color: Pallete.gradient2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:harmoniz/core/theme/theme.dart';
 import 'package:harmoniz/features/auth/view/pages/signup_pages.dart';
 
 void main() {
@@ -12,10 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: AppTheme.darkThemeMode,
       home: const SignupPages(),
     );
   }
